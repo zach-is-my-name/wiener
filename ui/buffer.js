42839ed8d1d41a2d69d44f8307f844551dac72d7
zach-is-my-name <zach.greco@gmail.com>
2021-10-14 00:06:07 -0400

    buffer with keys scaffold

--- ui/buffer.js
+++ ui/buffer.js
@@ -1,33 +1,205 @@
-const termkit = require('terminal-kit');
-const term = termkit.terminal;
-const {renderMarkdown} = require('../renderMarkdown.js'); 
+const blessed = require('blessed');
+const stripAnsi = require('strip-ansi');
+const { boxOptions, cursorOptions, inputFieldOptions } = require('./blessedOptions')
+const regexMarkdownHeading = /#{1,6} .+$/gm
+const regexMarkdownLink = /\[([^[]+)\]\(([^)]+)\)/gm
 
-/*
-const buffer = new termkit.ScreenBufferHD({
-  dst: term,
-  width: 120
-})
-*/
+const {renderMarkdown} = require('../renderMarkdown.js');
 
+const render = async () => {
+  const renderedMarkdown = await renderMarkdown(); 
+  // Create a screen object.
+  const screen = blessed.screen({
+    smartCSR: true,
+  });
 
+  screen.title = 'wiener';
 
+  // Create a box perfectly centered horizontally and vertically.
+  const box = blessed.box({
+    top: 'center',
+    left: 'center',
+    width: '100%',
+    height: '100%',
+    content: renderedMarkdown,
+    tags: true,
+    scrollable: true,
+  });
 
-const render = async ()  => {
-//term.fullscreen();
-term.grabInput();
-term.on( 'key' , function( name , matches , data ) {
-//    console.log( "'key' event:" , name ) ;
+  // Append our box to the screen.
+  screen.append(box);
 
-  switch(name) {
-		case 'CTRL_C' : term.processExit(0);
-    case 'j' : term.down(1); break;
-    case 'k' : term.up(1); break;
-    case 'h' : term.left(1); break;
-    case 'l' : term.right(1); break;
+  let cursorTop = 0;
+  let cursorLeft = 0;
+  let cursor = blessed.box(
+      Object.assign({}, cursorOptions, {
+        parent: box,
+        top: cursorTop,
+        left: cursorLeft,
+      }),
+  );
+  let isLoading = false;
+  const showHelp = () => return;
+  const showSearch = () => return;
+  
+  // Quit on Escape, q, or Control-C.
+  box.key(['escape', 'q', 'C-c','g','S-g','u','d','j','k','h','l','/'], async function(ch, key) {
+    //todo implement  --
+    // Mark --  m `(mark)
+    // Search -- / or c-f | pop-up box results
+    // Go to next search result -- n
+    // Go to prev search result -- N
+    // Foward word -- b
+    // Back word -- w
+    // Bookmark newsletter -- s | pop-up box, location, etc.  
+    // Open Bookmarks -- o |  pop-up box, select, rename
+    // Copy Line -- Y
+    // Copy selection -- y
+    // Clip Line - X | pop-up box, select destination clips,
+    // Clip Selection -- x | "
+    // Search Global -- C-S-f | pop-up box results 
+    if (isLoading) {
+      return
+    }
+    if (ch === 'escape' || 'q' || 'C-c') {
+      return process.exit(0);
+    } else if (ch === '?') {
+      //show help
+      cbShowHelp();
+    } else if (ch === '/') {
+      cbShowSearch();
+    } else {
+      //update cursor position
+      cursor.detach()
+      updateCoordinate(ch)
+      renderCursor()
+      screen.render()
+    }
+  });
+
+    box.on('click', async (mouse) => {
+      // move the cursor
+      cursor.detach()
+      const { x, y } = mouse
+      cursorTop = box.childBase + y - 1
+      cursorLeft = x - 1
+      renderCursor()
+      screen.render()
+
+      // check if the clicked chunk is a markdown link
+      await followLinkUnderCursor()
+    })
+
+  function updateCoordinate(input) {
+    if (input === 'j' || input === 'k') {
+      cursorTop = nextCursorPosition(
+        cursorTop,
+        input === 'j',
+        box.getScrollHeight(),
+        1,
+      )
+      box.scrollTo(cursorTop)
+    } else if (input === 'h' || input === 'l') {
+      cursorLeft = nextCursorPosition(
+        cursorLeft,
+        input === 'l',
+        box.width,
+        3,
+      )
+    } else if (input === 'g') {
+      cursorTop = 0
+      cursorLeft = 0
+      box.scrollTo(cursorTop)
+    } else if (input === 'S-g') {
+      cursorTop = box.getScreenLines().length - 1
+      cursorLeft = 0
+      box.scrollTo(cursorTop)
+    } else if (input === '0') {
+      cursorLeft = 0
+    } else if (input === '$') {
+      cursorLeft = (box.width) - 3
+    } else if (input === 'u') {
+      cursorTop += (box.height) - 2
+      if (cursorTop > box.getScrollHeight()) {
+        cursorTop = box.getScrollHeight() - 1
+      }
+      box.scrollTo(box.getScrollHeight())
+      box.scrollTo(cursorTop)
+    } else if (input === 'd') {
+      cursorTop -= (box.height) - 1
+      if (cursorTop < 0) {
+        cursorTop = 0
+      }
+      box.scrollTo(0)
+      box.scrollTo(cursorTop)
+    }
   }
-});
-const renderedMarkdown = await renderMarkdown();
-term(renderedMarkdown);
+
+  function nextCursorPosition( current, forward, maxLength, adjustment ) {
+    let position = current + (forward ? 1 : -1)
+    position = position < 0 ? 0 : position
+    position =
+      position > maxLength - adjustment ? maxLength - adjustment : position
+
+    return position
+  }
+
+  function renderCursor() {
+    cursor = blessed.box(
+      Object.assign({}, cursorOptions, {
+        parent: box,
+        top: cursorTop,
+        left: cursorLeft,
+      })
+    )
+  }
+
+  async function followLinkUnderCursor() {
+    // check if the chunk under the cursor is a markdown link
+    const lines = box.getScreenLines()
+    if (cursorTop >= lines.length) {
+      return
+    }
+    const before = lines.slice(0, cursorTop)
+    const cursorIndex = stripAnsi(before.join('')).length + cursorLeft
+    const cursorLine = stripAnsi(lines[cursorTop])
+    if (cursorLeft <= cursorLine.length) {
+      const text = stripAnsi(lines.join(''))
+      let match = regexMarkdownLink.exec(text)
+      while (match) {
+        const start = match.index
+        const end = start + match[0].length
+        if (start <= cursorIndex && cursorIndex < end) {
+          // jump to the link destination
+          await cbFollow(match[2])
+          break
+        }
+        match = regexMarkdownLink.exec(text)
+      }
+    }
+  }
+
+  function setLoadingState(isLoading) {
+    isLoading = isLoading
+  }
+
+  //controls for box
+  
+ box.key(['j','k','C-u','C-d', 'h', 'l', 'S-j'], function(ch, key) {
+   
+   switch(ch) {
+     case j: ; break;
+
+   }
+  
+ })
+
+  // Focus our element.
+  box.focus();
+
+  // Render the screen.
+  screen.render();
+
 }
 
 exports.render = render;
