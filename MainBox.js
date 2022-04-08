fb23aa67a5322c9c764f824ec7c91957f8c94b74
zach-is-my-name <zach.greco@gmail.com>
2022-04-08 19:20:24 -0400

    extract devlogger into devLogger; extract large functions into ui/functions.js

--- MainBox.js
+++ MainBox.js
@@ -16,10 +16,11 @@ const {getMarkdown} = require('./getMarkdown.js');
 import {formatBody} from './formatBody'
 import {formatHeader} from './formatHeader'
 import {formatPostFormat} from './formatPostFormat'
-import winston, {createLogger, transports}  from 'winston';
 import fs from 'fs'
+import {clickHandler, followLinkUnderCursor, keyHandler} from 'functions.js'
 const regexLink = /(https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b[-a-zA-Z0-9()@:%_\+.~#?&//=]*)/gm
 import useGetWien from './useGetWien'
+import {logger2} from './logger.js'
 
 const MainBox = (props) =>  {
   const [renderObj, setRenderObj] = useState(null)
@@ -42,10 +43,7 @@ const MainBox = (props) =>  {
   
 
   useEffect(() => {
-    //logger.info("click handler called; now in useEffect; state wasMouseClicked = ", {wasMouseClicked})
     if (wasMouseClicked) {
-      //logger.info("followLinkUnderCursor called in effect hook ")
-    // check if the clicked chunk is a markdown link
       followLinkUnderCursor()
     }
   }, [wasMouseClicked, cursorLeft, cursorTop]
@@ -69,154 +67,14 @@ const MainBox = (props) =>  {
   }, [cursorLeft, cursorTop, stateCallbackFlag]
   )
 
-  const clickHandler = (mouse) => {
-    // move the cursor
-    const { x, y } = mouse
-    setCursorLeft(x)
-    setCursorTop((mainBoxRef.current?.childBase + y))
-    toggleWasMouseClicked(state => !state)
-  }
 
-  const followLinkUnderCursor = () => {
-    // check if the chunk under the cursor is a markdown link
-            
-    const lines = mainBoxRef.current?.getScreenLines()
-    if (cursorTop >= lines.length) {
-      return
-    }
-    const before = lines.slice(0, cursorTop)
-    const cursorIndex = blessed.stripTags(before.join('')).length + cursorLeft
-    const cursorLine = blessed.stripTags(lines[cursorTop])
-    //logger.info({cursorLineLength:cursorLine.length, cursorLeft, cursorTop, wasMouseClicked})
-    if (cursorLeft <= cursorLine.length) {
-      const text = blessed.stripTags(lines.join(''))
-      let match = regexLink.exec(text)
-      //logger.info({match})
-      logger2.info({text}) 
-      while (match) {
-        const start = match.index
-        const end = start + match[0].length
-        //logger.info({start, end, cursorIndex, match1: match[1]}) 
-
-        if (start <= cursorIndex && cursorIndex < end) {
-          // jump to the link destination
-          openLink(match[1])
-          break
-        }
-
-        match = regexLink.exec(text)
-      }
-      toggleWasMouseClicked(false)}
-    }
 
   const openLink = async (link) => await open(`${link}`)     
 
-  const keyHandler = async (ch, key) => {
-    logger2.info({scrollPerc:mainBoxRef.current?.getScrollPerc(), scrollIndex:mainBoxRef.current?.getScroll() })
-    if (key.full === 'escape' || key.full === 'q' || key.full === 'C-c') {
-      return process.exit(0);
-
-    } else if (key.full === 'enter') {
-        await followLinkUnderCursor()
-    } else if (key.full === 'H') {
-        await newsletterBack()
-    } else if (key.full === 'L') {
-        await newsletterForward() 
-    } else {
-        updateCoordinate(key.full)
-    }
-
-    function nextCursorPosition( current, forward, maxLength, adjustment )  {
-      let position = current + (forward ? 1 : -1)
-      position = position < 0 ? 0 : position
-      position =
-        position > maxLength - adjustment ? maxLength - adjustment : position
-
-      return position
-    }
-    function nextTenXCursorPosition( current, forward, maxLength, adjustment )  {
-      let position = current + (forward ? 10 : -10)
-      position = position < 0 ? 0 : position
-      position =
-        position > maxLength - adjustment ? maxLength - adjustment : position
-
-      return position
-    }
-
-    function nextTwentyYCursorPosition( current, forward, maxLength, adjustment )  { 
-      let position = current + (forward ? 10 : -10)
-      position = position < 0 ? 0 : position
-      position =
-        position > maxLength - adjustment ? maxLength - adjustment : position
-
-      return position
-    }
-
-    function updateCoordinate (input)  {
-      if (input === 'j' || input === 'k') {
-        setCursorTop(nextCursorPosition(
-          cursorTop,
-          input === 'j',
-          mainBoxRef.current?.getScrollHeight(),
-          1,
-        ))
-        mainBoxRef.current?.scrollTo(cursorTop)
-      } else if (input === 'h' || input === 'l') {
-        setCursorLeft(nextCursorPosition(
-          cursorLeft,
-          input === 'l',
-          mainBoxRef.current?.width,
-          3,
-        ))
-      } else if  (input === 'w' || input === 'b') {
-        setCursorLeft(nextTenXCursorPosition(
-          cursorLeft,
-          input === 'w',
-          mainBoxRef.current?.width,
-          9,
-        ))
-      } else if (input === '{' || input === '}') {
-        setCursorTop(nextTwentyYCursorPosition(
-          cursorTop,
-          input === '{',
-          mainBoxRef.current?.getScrollHeight(),
-          9,
-        ))
-      }  else if (input === 'g') {
-        setCursorTop(0)
-        setCursorLeft(0)
-        setStateCallbackFlag(true)
-      } else if (input === 'S-g') {
-        setCursorTop(mainBoxRef.current?.getScreenLines().length - 1)
-        setCursorLeft(0)
-        setStateCallbackFlag(true)
-      } else if (input === '0') {
-        setCursorLeft(0)
-      } else if (input === '$') {
-        setCursorLeft((mainBoxRef.current?.width))
-        
-      } else if (input === "x") {
-        mainBoxRef.current?.setScrollPerc(100)
-      } else if (input === 'C-d') {
-        setCursorTop((cursorTop + mainBoxRef.current?.height) - 2)
-        if (cursorTop > mainBoxRef.current?.getScrollHeight()) {
-          setCursorTop(mainBoxRef.current?.getScrollHeight() - 1)
-        }
-        scrollToScrollHeightFlag.current = true;
-        setStateCallbackFlag(true)
-      } else if (input === 'C-u') {
-        setCursorTop(cursorTop - (mainBoxRef.current?.height)-2)
-        if (cursorTop < 0) {
-          setCursorTop(0)
-        }
-        scrollToZeroFlag.current = true
-        setStateCallbackFlag(true)
-      }
-    }
-  }
   const centeredHeader =`{center}${markdownHeader}`
   const clean = `\n{/}{/}\n\n`
   const leftBody = `{left}${markdownBody}{/left}` 
+
   return(
     <box 
     top={"top"}
@@ -248,23 +106,6 @@ const MainBox = (props) =>  {
 
 }
 
-winston.add(new winston.transports.File({
-  filename: '/home/zmg/Tinker/wiener/logs/errors.log',
-  handleExceptions: true,
-  handleRejections: true,
-}));
- 
-const logger = winston.createLogger({
-  transports: [
-    new winston.transports.File({ filename: '/home/zmg/Tinker/wiener/logs/utility.log' })
-  ]
-});
-
-const logger2 = winston.createLogger({
-  transports: [
-    new winston.transports.File({ filename: '/home/zmg/Tinker/wiener/logs/text.log' })
-  ]
-});
 
 export default MainBox
 
--- logger.js
+++ logger.js
@@ -1,11 +1,35 @@
 import { createRequire } from 'module';
 const require = createRequire(import.meta.url);
+import winston, {createLogger, transports}  from 'winston';
 
 import pkg  from 'winston';
 import winston from 'winston'
 const { createLogger, transports} = pkg
-const _logger = winston.createLogger({
+export const _logger = winston.createLogger({
     transports: [
           new winston.transports.File({ filename: '/home/zmg/Tinker/wiener/logs/combined.log' })]
 });
-export {_logger}
+
+
+winston.add(new winston.transports.File({
+  filename: '/home/zmg/Tinker/wiener/logs/errors.log',
+  handleExceptions: true,
+  handleRejections: true,
+}));
+ 
+export const logger = winston.createLogger({
+  transports: [
+    new winston.transports.File({ filename: '/home/zmg/Tinker/wiener/logs/utility.log' })
+  ]
+});
+
+export const logger2 = winston.createLogger({
+  transports: [
+    new winston.transports.File({ filename: '/home/zmg/Tinker/wiener/logs/text.log' })
+  ]
+});
+
+
+
+
+
