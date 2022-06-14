a3193f1dd81bc272670968f7d0cfe768504b7060
zach-is-my-name <zach.greco@gmail.com>
2022-06-13 23:48:07 -0500

    remove tracking from dists / logs

--- dist/ui/customHooks/index.js
+++
@@ -1,7 +0,0 @@
-//export * from "./functions.js"
-export * from "./ui/useClickHandler.js";
-export * from "./ui/useKeyHandler.js";
-export * from "./useGetWien.js";
-export * from "./ui/useMouseClick.js";
-export * from "./ui/useScroll.js";
-export * from "./useUpdateNewsletters.js";
--- dist/ui/format/format.js
+++
@@ -1,8 +0,0 @@
-import { formatBody } from './formatBody.js';
-import { formatHeader } from './formatHeader.js';
-import { formatPostFormat } from './formatPostFormat.js';
-export function format(newsletter) {
-  var header = formatHeader(newsletter);
-  var body = formatPostFormat(formatBody(newsletter));
-  return header + body;
-}
