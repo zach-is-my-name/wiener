11435c9c1471196a98a87d2a70550024854ef194
zach-is-my-name <zach.greco@gmail.com>
2022-12-18 20:38:57 -0500

    cleanup

--- src/db/db.js
+++ src/db/db.js
@@ -2,8 +2,8 @@ import { join, dirname } from 'path'
 import { Low, JSONFile } from 'lowdb'
 import { fileURLToPath } from 'url'
 
-const __dirname = dirname(fileURLToPath(import.meta.url)); //production db file
-// const __dirname = '/home/zmg/Tinker/wiener/src/db/'
+// const __dirname = dirname(fileURLToPath(import.meta.url)); //production db file
+const __dirname = '/home/zmg/Tinker/wiener/src/db/'
 const file = join(__dirname, 'db.json')
 const adapter = new JSONFile(file)
 const db = new Low(adapter)
