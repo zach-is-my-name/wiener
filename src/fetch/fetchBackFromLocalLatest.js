8e7903fd1515eb3dee96e9af5802931e7785f341
zach-is-my-name <zach.greco@gmail.com>
2023-01-30 14:20:33 -0500

    WIP debug

--- src/fetch/fetchBackFromLocalLatest.js
+++ src/fetch/fetchBackFromLocalLatest.js
@@ -1,6 +1,6 @@
 import {resolve} from 'import-meta-resolve'
 import chalk from 'chalk';
-import url from 'url'
+// import url from 'url'
 import got from 'got'
 import cheerio from 'cheerio'
 import {convertAndStore} from '../transform/convert.js';
@@ -8,16 +8,16 @@ import {fetchPermaLinkCurrent, getDateFromNewsletter} from '../utilities.js'
 import {loadNewsletterFromDb, addNextUrl} from '../db/db.js'
 import {logger} from '../devLog/logger.js' 
 logger.level = "debug"
-
+import fs from "fs"
 let count = 0
 let reqCount = 0
 
 export async function fetchBackFromLocalLatest(dateLatestPub) {
   let storedNewsletters = await loadNewsletterFromDb("all")
-    
-  await replaceBlankNextUrl(storedNewsletters)
+
+  await replaceBlankNextUrl(storedNewsletters, dateLatestPub)
   let targetUrl = await fetchPermaLinkCurrent()
-  
+
   let count = 0 
 
   while (targetUrl) {
@@ -27,21 +27,50 @@ export async function fetchBackFromLocalLatest(dateLatestPub) {
       targetUrl = newsletterObj.prevUrl
     } else {
       logger.debug(`targetUrl not found: fetching: ${targetUrl}`)
+        debugger;
+      /*
       const writtenNewsletterObj = await fetchAndAdd(targetUrl) 
+      */
+      /* begin -------------- */
+        const url = targetUrl
+      let fetchedNewsletter
+      try {
+        fetchedNewsletter = await got(url).text()
+      } catch (error) {
+        logger.debug("error", error)
+      }
+      // fs.writeFileSync("/home/zmg/tmp/got.txt", fetchedNewsletter)
+        
+        reqCount++
+        
+        const $ = cheerio.load(fetchedNewsletter)
+        const $url = $('link[rel="canonical"]').attr('href')
+        let prevUrl = $('.nav-previous').children('a').attr('href');
+        let nextUrl = $('.nav-next').children('a').attr('href');
+        !nextUrl ? nextUrl = "" : nextUrl = nextUrl
+        url === "https://weekinethereumnews.com/january-4-2019/" ? prevUrl = "https://weekinethereumnews.com/december-28-2018/" : prevUrl = prevUrl
+        logger.debug({length: fetchedNewsletter.length, type: typeof fetchedNewsletter, prevUrl, nextUrl})
+        debugger
+        const storedNewsletterObj = await convertAndStore(fetchedNewsletter, url, prevUrl, nextUrl) 
+        logger.debug(`written newsletter; next to check: ${storedNewsletterObj.prevUrl}`)
+        logger.debug("storedNewsletterObj", storedNewsletterObj) 
 
-      logger.debug("writtenNewsletterObj", writtenNewsletterObj)  
-      targetUrl = writtenNewsletterObj.prevUrl 
-      logger.debug(`end loop iteration. new targetUrl ${targetUrl}`)
-    }
-    count++
-    if (!targetUrl) {
-      logger.debug("no target url", targetUrl)
-      break
+
+
+
+        /* end -------------- */
+        logger.debug("storedNewsletterObj ", storedNewsletterObj )  
+        targetUrl = storedNewsletterObj .prevUrl 
+        logger.debug(`end loop iteration. new targetUrl ${targetUrl}`)
+      count++
+      if (!targetUrl) {
+        logger.debug("no target url", targetUrl)
+        break
+      }
     }
   }
-
-
-  async function replaceBlankNextUrl(newsletters) {
+}
+async function replaceBlankNextUrl(newsletters, dateLatestPub) {
     if (newsletters.length > 1) {
       for (let i = 0; i < newsletters.length; i++) {
         if (!newsletters[i].nextUrl && newsletters[i].date !== dateLatestPub) {
@@ -50,26 +79,24 @@ export async function fetchBackFromLocalLatest(dateLatestPub) {
       }
     }
   }
+  // async function fetchAndAdd(url) {
+  //   if (url) {
+  //     let fetchedNewsletter =  await got(url).text().catch(e => new Error(e))
+  //     reqCount++
+  //     if (typeof fetchedNewsletter !== 'string') return new Error(`error on fetched newsletter, value: ${fetchedNewsletter}`)
+  //     const $ = cheerio.load(fetchedNewsletter)
+  //     const $url = $('link[rel="canonical"]').attr('href')
+  //     let prevUrl = $('.nav-previous').children('a').attr('href');
+  //     let nextUrl = $('.nav-next').children('a').attr('href');
+  //     !nextUrl ? nextUrl = "" : nextUrl = nextUrl
+  //     url === "https://weekinethereumnews.com/january-4-2019/" ? prevUrl = "https://weekinethereumnews.com/december-28-2018/" : prevUrl = prevUrl
 
-}
-  async function fetchAndAdd(url) {
-    if (url) {
-      let fetchedNewsletter =  await got(url).text().catch(e => new Error(e))
-      reqCount++
-      if (typeof fetchedNewsletter !== 'string') return new Error(`error on fetched newsletter, value: ${fetchedNewsletter}`)
-      const $ = cheerio.load(fetchedNewsletter)
-      const $url = $('link[rel="canonical"]').attr('href')
-      let prevUrl = $('.nav-previous').children('a').attr('href');
-      let nextUrl = $('.nav-next').children('a').attr('href');
-      !nextUrl ? nextUrl = "" : nextUrl = nextUrl
-      url === "https://weekinethereumnews.com/january-4-2019/" ? prevUrl = "https://weekinethereumnews.com/december-28-2018/" : prevUrl = prevUrl
-
-      const storedNewsletterObj = await convertAndStore(fetchedNewsletter, url, prevUrl, nextUrl) 
-      // logger.debug(`written newsletter; next to check: ${storedNewsletterObj.prevUrl}`)
-      logger.debug("storedNewsletterObj", storedNewsletterObj) 
-      return storedNewsletterObj 
-    }
-    logger.debug("early return fetchAndAdd")
-    return  
-  }
+  //     const storedNewsletterObj = await convertAndStore(fetchedNewsletter, url, prevUrl, nextUrl) 
+  //     // logger.debug(`written newsletter; next to check: ${storedNewsletterObj.prevUrl}`)
+  //     logger.debug("storedNewsletterObj", storedNewsletterObj) 
+  //     return storedNewsletterObj 
+  //   }
+  //   logger.debug("early return fetchAndAdd")
+  //   return  
+  // }
 
